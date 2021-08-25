8caa9b2246fbe278edba39058b7038bb8daa527b
Fayaze H. Pantho <60003361+Fayazexo@users.noreply.github.com>
2021-08-25 16:17:50 +0600

    Styled Components and Babel Config

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <head>
+        <title>Chipaaaaaa</title>
+      </head>
+      <h1>Come into the mainkar chipa</h1>
+      <Image
+        width="1170px"
+        height="1447px"
+        src="/images/nafiurrn.jpg"
+        alt="nafirurn"
+      ></Image>
+    </>
+  );
+}
